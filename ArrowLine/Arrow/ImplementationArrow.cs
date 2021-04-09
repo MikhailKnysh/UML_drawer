6f87c64fea40f0f6c54654c4186604b6628fe06e
Disic <Nickincorname@gmail.com>
2021-04-09 22:11:29 +0300

    Added delta for correct draw arrows

--- ArrowLine/Arrow/ImplementationArrow.cs
+++ ArrowLine/Arrow/ImplementationArrow.cs
@@ -2,6 +2,7 @@
 using ArrowLine.Line;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System;
 
 namespace ArrowLine.Arrow
 {
@@ -9,6 +10,15 @@ namespace ArrowLine.Arrow
     {
         public override void Draw(Pen pen, Graphics graphics)
         {
+            if (Math.Abs(_startPoint.X - _endPoint.X) < 20)
+            {
+                _endPoint.X = _startPoint.X;
+            }
+            else if (Math.Abs(_startPoint.Y - _endPoint.Y) < 20)
+            {
+                _endPoint.Y = _startPoint.Y;
+            }
+
             AbstractArrow arrowCap = new CloseCapArrow(_startPoint, _endPoint);
             arrowCap.Draw(pen, graphics);
 
--- ArrowLine/Arrow/InheritanceArrow.cs
+++ ArrowLine/Arrow/InheritanceArrow.cs
@@ -1,6 +1,7 @@
 ﻿using ArrowLine.CapArrow;
 using ArrowLine.Line;
 using System.Drawing;
+using System;
 
 namespace ArrowLine.Arrow
 {
@@ -8,8 +9,18 @@ namespace ArrowLine.Arrow
     {
         public override void Draw(Pen pen, Graphics graphics)
         {
+            if (Math.Abs(_startPoint.X - _endPoint.X) < 20)
+            {
+                _endPoint.X = _startPoint.X;
+            }
+            else if (Math.Abs(_startPoint.Y - _endPoint.Y) < 20)
+            {
+                _endPoint.Y = _startPoint.Y;
+            }
+
             AbstractArrow line = new SolidLineArrow(_startPoint, _endPoint);
-            line.Draw(pen,graphics);
+            line.Draw(pen, graphics);
+
             AbstractArrow arrowCap = new CloseCapArrow(_startPoint, _endPoint);
             arrowCap.Draw(pen, graphics);
         }
